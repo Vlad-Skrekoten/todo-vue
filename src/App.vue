06b3d42f8e4307634c20d97f930e8e36d6a1f065
Vlad <vladskrekotenconter@gmail.com>
2023-08-30 19:13:41 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,20 @@
 export default {
   data() {
     return {
+      error: '',
       valueInput: '',
+      editingtask: false,
       Dolist: [],
       Completelist: []
     };
+  },
+
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
   },
 
   methods: {
@@ -13,22 +23,39 @@ export default {
       this.valueInput = event.target.value;
     },
     addTask() {
-      if (this.valueInput === '') { return };
+      if (this.valueInput.trim().length < 2) {
+        this.error = "Error";
+        return;
+      };
       this.Dolist.push({
         title: this.valueInput,
         id: Math.random()
       });
       this.valueInput = '';
+      this.error = '';
     },
     doCheck(index, type) {
+
       if (type === 'need') {
         const completeMask = this.Dolist.splice(index, 1);
         this.Completelist.push(...completeMask);
       }
+
       else {
         const noCompleteMask = this.Completelist.splice(index, 1);
         this.Dolist.push(...noCompleteMask);
       }
+    },
+
+
+
+    editTodoTask() {
+      this.Dolist.editingtask = true;
+    },
+
+    doneEditTask() {
+      this.Dolist.editingtask = false;
+
     },
 
     remove(index, type) {
@@ -42,22 +69,28 @@ export default {
 <template>
   <div class="container">
     <header>
-      <div class="title">All Tasks</div>
+      <div class="title">ToDoList</div>
       <div class="send_menu">
         <input class="window" v-bind:value="valueInput" v-on:input="hanlyInput" v-on:keypress.enter="addTask" type="text"
           placeholder="Enter task">
         <button class="send" v-on:click="addTask">Add</button>
       </div>
+      <div class="error">{{ error }}</div>
     </header>
-    <main>
-      <div class="title_task">Task</div>
+    <main class="main">
+      <div class="block_task">
+        <div class="title_task">Tasks</div>
+        <div class="numbers_task">{{ Dolist.length }}</div>
+      </div>
       <ul class="mask-list">
         <li v-for="(mask, index) in  Dolist " :key="mask.id">
           <label class="cont_check">
             <input type="checkbox" v-on:change="doCheck(index, 'need')">
             <div class="checkmark"></div>
           </label>
-          <span>{{ mask.title }}</span>
+          <div class="result" v-if="!Dolist.editingtask" @dblclick="editTodoTask()">{{ mask.title }}</div>
+          <div v-else class="item-edit"><input class="editTask" type="text" v-model="mask.title" @blur="doneEditTask()"
+              @keyup.enter="doneEditTask()" v-focus></div>
           <button class="delete" v-on:click="remove(index, 'need')"><svg xmlns="http://www.w3.org/2000/svg" width="24"
               height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
               <path
@@ -66,14 +99,19 @@ export default {
             </svg></button>
         </li>
       </ul>
-      <div class="title_done">Done</div>
+      <div class="block_done">
+        <div class="title_done">Done</div>
+        <div class="numbers_done">{{ Completelist.length }}</div>
+      </div>
       <ul class="mask_list complete_list">
         <li v-for="(mask, index) in Completelist" :key="mask.id">
           <label class="cont_check">
             <input checked="checked" type="checkbox" v-on:change="doCheck(index, 'complete')">
             <div class="checkmark"></div>
           </label>
-          <span>{{ mask.title }}</span>
+          <div class="block-task_input">
+            <div class="resultdone"> {{ mask.title }} </div>
+          </div>
           <button class="delete" v-on:click="remove(index, 'complete')"><svg xmlns="http://www.w3.org/2000/svg" width="24"
               height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
               <path
@@ -140,11 +178,6 @@ export default {
   }
 }
 
-main {
-  width: 100%;
-  max-width: 400px;
-}
-
 li {
   padding: 10px;
   display: grid;
@@ -175,13 +208,30 @@ li {
   }
 }
 
-main {
+.error {
+  margin-top: 20px;
+  font-weight: 500;
+  font-size: 20px;
+  text-align: center;
+  color: $red;
+}
+
+.main {
+  width: 100%;
+  max-width: 400px;
   margin-top: 30px;
   padding: 30px;
   background-color: $grey;
   display: grid;
   gap: 20px;
   border-radius: 20px;
+}
+
+.block_task,
+.block_done {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .title_task,
@@ -202,5 +252,23 @@ main {
   &:active {
     transform: scale(1);
   }
+}
+
+.editTask {
+  background-color: $white;
+  border-radius: 20px;
+  padding: 5px;
+  border: none;
+  font-size: 15px;
+}
+
+.result {
+  max-width: 200px;
+  height: max-content;
+}
+
+.resultdone {
+  max-width: 200px;
+  height: max-content;
 }
 </style>
